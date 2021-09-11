41bf759904889b595eb36ec05794f4ed68490438
Sofía Esquivel <sofia.esquivel@lcieducation.net>
2021-09-10 18:09:36 -0600

    step two complete

---
+++ loopChallenges.js
@@ -0,0 +1,19 @@
+/* 1. Print odds 1-20 */ 
+for (let i = 1; i < 20; i++){
+    if (i % 2 != 0)
+    console.log (i);
+} 
+
+/* 2. Decreasing Multiples of 3 from 100 down to 0 */
+
+for (let i = 100; i > 0; i--){
+    if (i % 3 === 0)
+    console.log (i);
+}
+
+/* 3. Print the sequence 4, 2.5, 1, -0.5, -2, -3.5 */
+
+
+/* 4. Sigma */
+
+/* 5. Factorial */
